6c7184d0cfd30692a1634d6002e88be38ee61671
Minakian <stephenminakian@gmail.com>
2017-03-10 09:02:48 -0700

    Initial Project Submission

---
+++ model.py
@@ -0,0 +1,75 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('../Driving_Data2/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+correction = 0.2
+for line in lines:
+    source_path = line[0]
+    filename_center = source_path.split('/')[-1]
+    source_path = line[1]
+    filename_left = source_path.split('/')[-1]
+    source_path = line[2]
+    filename_right = source_path.split('/')[-1]
+    current_path = '../Driving_Data2/IMG/' + filename_center
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+    images.append(cv2.flip(image,1))
+    measurements.append(measurement*-1.0)
+
+    current_path = '../Driving_Data2/IMG/' + filename_right
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement - correction)
+    images.append(cv2.flip(image,1))
+    measurements.append((measurement - correction)*-1.0)
+    current_path = '../Driving_Data2/IMG/' + filename_left
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+    images.append(cv2.flip(image,1))
+    measurements.append((measurement + correction)*-1.0)
+
+
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5)) #, input_shape=(160,320,3)))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+#model.add(MaxPooling2D())
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.5))
+#model.add(MaxPooling2D())
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch=5)
+
+model.save('model.h5')
+exit()
