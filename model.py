f546e50c4d31a02dadf2fdf7c8f4ced73316bd91
Minakian <stephenminakian@gmail.com>
2017-03-10 10:25:47 -0700

    Added Comments

--- model.py
+++ model.py
@@ -12,34 +12,39 @@ images = []
 measurements = []
 correction = 0.2
 for line in lines:
+    # Center image path
     source_path = line[0]
     filename_center = source_path.split('/')[-1]
+    # Left image path
     source_path = line[1]
     filename_left = source_path.split('/')[-1]
+    # Right image path
     source_path = line[2]
     filename_right = source_path.split('/')[-1]
+    # Load center images
     current_path = '../Driving_Data2/IMG/' + filename_center
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
-    images.append(cv2.flip(image,1))
-    measurements.append(measurement*-1.0)
-
+    images.append(cv2.flip(image,1))    # Add flipped image
+    measurements.append(measurement*-1.0)    # Add flipped steering angle
+    # Load left images
     current_path = '../Driving_Data2/IMG/' + filename_right
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement - correction)
-    images.append(cv2.flip(image,1))
-    measurements.append((measurement - correction)*-1.0)
+    images.append(cv2.flip(image,1))    # Add flipped image
+    measurements.append((measurement - correction)*-1.0)    # Add flipped steering angle
+    # Load right Images
     current_path = '../Driving_Data2/IMG/' + filename_left
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
-    images.append(cv2.flip(image,1))
-    measurements.append((measurement + correction)*-1.0)
+    images.append(cv2.flip(image,1))    # Add flipped image
+    measurements.append((measurement + correction)*-1.0)    # Add flipped steering angle
 
 
 
@@ -51,14 +56,13 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+# Convolutional Model
 model = Sequential()
 model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5)) #, input_shape=(160,320,3)))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-#model.add(MaxPooling2D())
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
 model.add(Dropout(0.5))
-#model.add(MaxPooling2D())
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
@@ -68,8 +72,10 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+# Train the network
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch=5)
 
+#Save the model
 model.save('model.h5')
 exit()
